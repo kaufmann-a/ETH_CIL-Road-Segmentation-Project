3e8ec826a60ea666c45073e0aafe8747bc54f369
braunjon <25931090+braunjon@users.noreply.github.com>
2021-03-23 22:17:46 +0100

    Improve metrics

--- core/model/res_unet_plus.py
+++ core/model/res_unet_plus.py
@@ -5,7 +5,7 @@
 import torch
 import torch.nn as nn
 
-from modules import (
+from core.model.modules import (
     ResidualConv,
     ASPP,
     AttentionBlock,
--- core/train.py
+++ core/train.py
@@ -1,13 +1,15 @@
 import albumentations as A
 import torch
 import torch.optim as optim
+import torchmetrics as torchmetrics
 from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm  # used to show progress bar
 
 from core.dataset.simple_dataset import RoadSegmentationSimpleDataset
 # Hyperparameters etc.
 from core.model.res_unet import ResUnet
-from core.utils.metrics import get_accuracy, BCEDiceLoss
+from core.model.res_unet_plus import ResUnetPlusPlus
+from core.utils.metrics import BCEDiceLoss
 
 LEARNING_RATE = 1e-3
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -28,9 +30,11 @@ def evaluate(model, batch_size, loss_fn, data_loader):
     Evaluate model on validation data.
     """
     model.eval()
-    total_loss, total_accuracy, total_dice_score = 0., 0., 0.
-    num_correct = 0
-    num_pixels = 0
+    total_loss, total_dice_score = 0., 0.
+
+    # initialize accuracy metric
+    accuracy = torchmetrics.Accuracy()
+    accuracy.to(DEVICE)
 
     with torch.no_grad():
         for i, (image, label) in enumerate(data_loader):
@@ -39,21 +43,25 @@ def evaluate(model, batch_size, loss_fn, data_loader):
             output = model(image)
             loss = loss_fn(output, label)  # compute loss
 
+            # update loss
+            # TODO create loss metric class
             total_loss += loss.item()
 
-            # TODO fix scores
+            # update accuracy
+            accuracy.update(output, label.int())
+
+            # update dice score
+            # TODO create dice score metrics class
             preds = torch.sigmoid(output)
-            preds = (preds > 0.5).float()
-            num_correct += (preds == label).sum()
-            num_pixels += torch.numel(preds)
+            preds = (preds >= 0.5).float()
 
             total_dice_score += (2 * (preds * label).sum()) / (
                     (preds + label).sum() + 1e-8
             )
 
     total_loss /= len(data_loader)
-    total_accuracy = num_correct / num_pixels
-    total_dice_score /= len(data_loader)  # * batch_size
+    total_accuracy = accuracy.compute()
+    total_dice_score /= len(data_loader)
 
     return total_loss, total_accuracy, total_dice_score
 
@@ -65,7 +73,11 @@ def train_step(model, batch_size, optimizer, loss_fn, data_loader):
     loop = tqdm(data_loader)
 
     model.train()
-    total_loss, total_accuracy, total_dice_score = 0., 0., 0.
+    total_loss, total_dice_score = 0., 0.
+
+    # initialize accuracy metric
+    accuracy = torchmetrics.Accuracy()
+    accuracy.to(DEVICE)
 
     for i, (image, label) in enumerate(loop):
         image, label = image.to(DEVICE), label.float().unsqueeze(1).to(DEVICE)
@@ -79,21 +91,28 @@ def train_step(model, batch_size, optimizer, loss_fn, data_loader):
         loss.backward()
         optimizer.step()  # perform update
 
-        # evaluation
+        # update loss
+        # TODO create loss metric class
         total_loss += loss.item()
 
-        # TODO fix scores
-        acc, dice = get_accuracy(output, label)
+        # update accuracy
+        accuracy.update(output, label.int())
 
-        total_accuracy += acc
-        total_dice_score += dice
+        # update dice score
+        # TODO create dice score metrics class
+        preds = torch.sigmoid(output)
+        preds = (preds >= 0.5).float()
+
+        total_dice_score += (2 * (preds * label).sum()) / (
+                (preds + label).sum() + 1e-8
+        )
 
         # update tqdm loop
         loop.set_postfix(loss=loss.item())
 
     total_loss /= len(data_loader)
-    total_accuracy /= (len(data_loader) * batch_size)
-    total_dice_score /= len(data_loader) * batch_size
+    total_accuracy = accuracy.compute()
+    total_dice_score /= len(data_loader)
 
     return total_loss, total_accuracy, total_dice_score
 
@@ -136,7 +155,7 @@ def main():
                                                   shuffle=True,
                                                   pin_memory=True)
 
-    model = ResUnet(3).to(DEVICE)
+    model = ResUnetPlusPlus(3).to(DEVICE)
     loss = BCEDiceLoss()
     optimizer = optim.Adam(model.parameters(), LEARNING_RATE)
 
@@ -148,11 +167,12 @@ def main():
         # evaluate
         val_loss, val_accuracy, val_dice_score = evaluate(model=model, loss_fn=loss, data_loader=val_data_loader,
                                                           batch_size=BATCH_SIZE)
-        # TODO fix scores
-        print(f"[Epoch {epoch}] - Training : accuracy = {train_accuracy},"
-              f" loss = {train_loss}, dice score = {train_dice_score}",
-              end="\n ")
-        print(f"\t\tValidation : accuracy = {val_accuracy}, loss = {val_loss}, dice score = {val_dice_score}")
+
+        print(f"\n[Epoch {epoch}] - Training :   accuracy = {train_accuracy:.5f},"
+              f" loss = {train_loss:.5f}, dice score = {train_dice_score:.5f}")
+
+        print(f"......... - Validation : accuracy = {val_accuracy:.5f},"
+              f" loss = {val_loss:.5f}, dice score = {val_dice_score:.5f}")
 
 
 if __name__ == '__main__':
