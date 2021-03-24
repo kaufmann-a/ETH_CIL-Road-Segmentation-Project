30a0d09b33f96dd553618c91cf8722ccf180e2d3
braunjon <25931090+braunjon@users.noreply.github.com>
2021-03-24 21:37:40 +0100

    Improve save_predictions_as_images in case the model already outputs probabilities

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -4,20 +4,38 @@ import torch
 import torchvision
 
 
-def save_predictions_as_imgs(loader, model, folder="../data/train-predictions", device="cuda"):
+def save_predictions_as_imgs(loader, model, folder="../data/train-predictions", device="cuda", is_prob=True):
+    """
+    Save the predictions of the model as images.
+
+    :param loader: the data loader to use
+    :param model: the model to get the predictions
+    :param folder: output folder to save the images in (is created if it does not exist)
+    :param device: cuda/cpu
+    :param is_prob: True = The output of the model are probabilities.
+    :return:
+    """
     if not os.path.exists(folder):
         os.makedirs(folder)
+
     model.eval()
+
     for idx, (x, y) in enumerate(loader):
         x = x.to(device=device)
         y = y.to(device).float().unsqueeze(1)
-        with torch.no_grad():
-            preds = torch.sigmoid(model(x))
-            preds = (preds > 0.5).float()
-            torchvision.utils.save_image(
-                preds, f"{folder}/pred_{idx}.png"
-            )
 
+        with torch.no_grad():
+            preds = model(x)
+            if not is_prob:
+                preds = torch.sigmoid(preds)
+
+            # probabilities to 0/1
+            preds = (preds >= 0.5).float()
+
+            # save predictions
+            torchvision.utils.save_image(preds, f"{folder}/pred_{idx}.png")
+
+            # save truth
             torchvision.utils.save_image(y, f"{folder}/pred_y_{idx}.png")
 
     model.train()
