0ee87736b5f7c1dcb5051a4cdf72c0af841ffe08
braunjon <25931090+braunjon@users.noreply.github.com>
2021-03-24 21:08:18 +0100

    Added annotations, comments & parameter names

--- core/model/res_unet.py
+++ core/model/res_unet.py
@@ -9,35 +9,51 @@ from core.model.modules import ResidualConv, Upsample
 
 
 class ResUnet(nn.Module):
+    """
+    Based on https://arxiv.org/pdf/1711.10684.pdf
+
+    """
+
     def __init__(self, channel, filters=[64, 128, 256, 512]):
         super(ResUnet, self).__init__()
 
+        # Encoding
+        # level 1
         self.input_layer = nn.Sequential(
-            nn.Conv2d(channel, filters[0], kernel_size=3, padding=1),
+            nn.Conv2d(channel, filters[0], kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(filters[0]),
             nn.ReLU(),
-            nn.Conv2d(filters[0], filters[0], kernel_size=3, padding=1),
+            nn.Conv2d(filters[0], filters[0], kernel_size=3, stride=1, padding=1),
         )
         self.input_skip = nn.Sequential(
-            nn.Conv2d(channel, filters[0], kernel_size=3, padding=1)
+            nn.Conv2d(channel, filters[0], kernel_size=3, stride=1, padding=1)
         )
 
-        self.residual_conv_1 = ResidualConv(filters[0], filters[1], 2, 1)
-        self.residual_conv_2 = ResidualConv(filters[1], filters[2], 2, 1)
+        # Level 2
+        self.residual_conv_1 = ResidualConv(filters[0], filters[1], stride=2, padding=1)
+        # Level 3
+        self.residual_conv_2 = ResidualConv(filters[1], filters[2], stride=2, padding=1)
 
-        self.bridge = ResidualConv(filters[2], filters[3], 2, 1)
+        # Bridge
+        # Level 4
+        self.bridge = ResidualConv(filters[2], filters[3], stride=2, padding=1)
 
-        self.upsample_1 = Upsample(filters[3], filters[3], 2, 2)
-        self.up_residual_conv1 = ResidualConv(filters[3] + filters[2], filters[2], 1, 1)
+        # Decoding
+        # Level 5
+        self.upsample_1 = Upsample(filters[3], filters[3], kernel=2, stride=2)
+        self.up_residual_conv1 = ResidualConv(filters[3] + filters[2], filters[2], stride=1, padding=1)
 
-        self.upsample_2 = Upsample(filters[2], filters[2], 2, 2)
-        self.up_residual_conv2 = ResidualConv(filters[2] + filters[1], filters[1], 1, 1)
+        # Level 6
+        self.upsample_2 = Upsample(filters[2], filters[2], kernel=2, stride=2)
+        self.up_residual_conv2 = ResidualConv(filters[2] + filters[1], filters[1], stride=1, padding=1)
 
-        self.upsample_3 = Upsample(filters[1], filters[1], 2, 2)
-        self.up_residual_conv3 = ResidualConv(filters[1] + filters[0], filters[0], 1, 1)
+        # Level 7
+        self.upsample_3 = Upsample(filters[1], filters[1], kernel=2, stride=2)
+        self.up_residual_conv3 = ResidualConv(filters[1] + filters[0], filters[0], stride=1, padding=1)
 
+        # Output
         self.output_layer = nn.Sequential(
-            nn.Conv2d(filters[0], 1, 1, 1),
+            nn.Conv2d(filters[0], out_channels=1, kernel_size=1, stride=1),
             nn.Sigmoid(),
         )
 
