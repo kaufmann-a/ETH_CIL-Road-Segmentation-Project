cb36429fab986ffaecb21f8aa0a577b8ed8f0f2f
braunjon <25931090+braunjon@users.noreply.github.com>
2021-03-30 23:10:14 +0200

    Added dice loss functions

--- road_segmentation_main/source/lossfunctions/lossfunctionfactory.py
+++ road_segmentation_main/source/lossfunctions/lossfunctionfactory.py
@@ -8,11 +8,10 @@ Builds a torch loss function from configuration.
 __author__ = 'Andreas Kaufmann, Jona Braun, Frederike Lübeck, Akanksha Baranwal'
 __email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, fluebeck@student.ethz.ch, abaranwal@student.ethz.ch"
 
-
-import torch
 import torch.nn as nn
 
 from source.configuration import Configuration
+from source.lossfunctions.lossfunctions import DiceLoss, DiceBCELoss
 
 
 class LossFunctionFactory(object):
@@ -27,10 +26,23 @@ class LossFunctionFactory(object):
 
     @staticmethod
     def bce_with_logits_loss():
+        """
+        Uses intern a sigmoid to get probabilities.
+        """
         return nn.BCEWithLogitsLoss()
+
+    @staticmethod
+    def dice_bce_loss():
+        return DiceBCELoss()
+
+    @staticmethod
+    def dice_loss():
+        return DiceLoss()
 
     @staticmethod
     def get_members():
         return {
-            'bce_with_logits_loss': LossFunctionFactory.bce_with_logits_loss
+            'bce_with_logits_loss': LossFunctionFactory.bce_with_logits_loss,
+            'dice_bce_loss': LossFunctionFactory.dice_bce_loss,
+            'dice_loss': LossFunctionFactory.dice_loss
         }
