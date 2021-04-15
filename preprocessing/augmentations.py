1cd31aa82d4cb0d115d3a0e4b227a6966006196f
braunjon <25931090+braunjon@users.noreply.github.com>
2021-04-15 22:46:52 +0200

    Make rotations deterministic

--- preprocessing/augmentations.py
+++ preprocessing/augmentations.py
@@ -2,21 +2,46 @@ import os
 
 import albumentations as A
 import cv2
+import numpy as np
+from albumentations.augmentations import functional as F
+
+
+class Rotation(A.DualTransform):
+    """
+    Rotates the image by a fixed angle of 90deg * factor.
+    """
+
+    def __init__(self, factor=1, always_apply=True, p=1.0):
+        super().__init__(always_apply=always_apply, p=p)
+        self.factor = factor
+
+    def apply(self, img, **params):
+        return np.ascontiguousarray(np.rot90(img, self.factor))
+
+    def get_params(self):
+        return {}
+
+    def apply_to_bbox(self, bbox, **params):
+        return F.bbox_rot90(bbox, self.factor, **params)
+
+    def apply_to_keypoint(self, keypoint, **params):
+        return F.keypoint_rot90(keypoint, self.factor, **params)
+
+    def get_transform_init_args_names(self):
+        return ()
+
 
 # desired transformations
-rotate_90 = A.Compose([A.RandomRotate90(always_apply=True)])
-rotate_180 = A.Compose([A.RandomRotate90(always_apply=True),
-                        A.RandomRotate90(always_apply=True)])
-rotate_270 = A.Compose([A.RandomRotate90(always_apply=True),
-                        A.RandomRotate90(always_apply=True),
-                        A.RandomRotate90(always_apply=True)])
+rotate_90 = A.Compose([Rotation(factor=1, always_apply=True)])
+rotate_180 = A.Compose([Rotation(factor=2, always_apply=True)])
+rotate_270 = A.Compose([Rotation(factor=3, always_apply=True)])
 
 flip_hor = A.Compose([A.HorizontalFlip(always_apply=True)])
 flip_hor_90 = A.Compose([A.HorizontalFlip(always_apply=True),
-                         A.RandomRotate90(always_apply=True)])
+                         Rotation(factor=1, always_apply=True)])
 flip_ver = A.Compose([A.VerticalFlip(always_apply=True)])
 flip_ver_90 = A.Compose([A.VerticalFlip(always_apply=True),
-                         A.RandomRotate90(always_apply=True)])
+                         Rotation(factor=1, always_apply=True)])
 
 crop_random = A.Compose([A.RandomCrop(always_apply=True, width=400, height=400)])
 rotate_random = A.Compose([A.Rotate(always_apply=True)])
@@ -58,22 +83,21 @@ if __name__ == '__main__':
         if filename.endswith(".png"):
             image = cv2.imread(os.path.join(IMAGE_DIR, filename))
             mask = cv2.imread(os.path.join(MASK_DIR, filename))
+            print(f"{filename}")
 
             # apply the transformations
             # rotations
-            print("...rotations")
             transform_and_save(image, mask, name="rotate_90", transform=rotate_90)
             transform_and_save(image, mask, name="rotate_180", transform=rotate_180)
             transform_and_save(image, mask, name="rotate_270", transform=rotate_270)
 
             # flipped image plus rotations
-            print("...flipping and rotations")
             transform_and_save(image, mask, name="flip_hor", transform=flip_hor)
             transform_and_save(image, mask, name="flip_hor_90", transform=flip_hor_90)
             transform_and_save(image, mask, name="flip_ver", transform=flip_ver)  # flipped + 180
             transform_and_save(image, mask, name="flip_ver_90", transform=flip_ver_90)  # flipped + 270
 
+            # TODO fix random seed
             # random
-            print("...random augmentations")
             transform_and_save(image, mask, name="crop_random", transform=crop_random)
             transform_and_save(image, mask, name="rotate_random", transform=rotate_random)
