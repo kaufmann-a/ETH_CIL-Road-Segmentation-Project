7c9f40328e13ca069c5508256f02142ebca3522b
frederikeluebeck <frederike.luebeck@web.de>
2021-05-11 23:39:40 +0200

    bash script to rename masks config updates update configs crop additional images from github

---
+++ preprocessing/crop_images.py
@@ -0,0 +1,47 @@
+import os
+
+import albumentations as A
+import cv2
+import numpy as np
+from albumentations.augmentations import functional as F
+
+
+
+crop = A.Compose([A.CenterCrop(always_apply=True, width=400, height=400)])
+
+IMAGE_DIR = "../data/training/github_jkf/images"
+MASK_DIR = "../data/training/github_jkf/masks"
+DIR = "../data/training"
+
+
+def transform_and_save(image, mask, name, transform):
+    transformed = transform(image=image, mask=mask)
+    aug_filename = filename
+
+    aug_image_dir = os.path.join(DIR, name, "images")
+    aug_mask_dir = os.path.join(DIR, name, "masks")
+
+    if not os.path.exists(aug_image_dir):
+        os.makedirs(os.path.join(DIR, name, "images"))
+    if not os.path.exists(aug_mask_dir):
+        os.makedirs(os.path.join(DIR, name, "masks"))
+
+    cv2.imwrite(
+        filename=os.path.join(aug_image_dir, aug_filename),
+        img=transformed["image"],
+    )
+
+    cv2.imwrite(
+        filename=os.path.join(aug_mask_dir, aug_filename),
+        img=transformed["mask"],
+    )
+
+
+if __name__ == '__main__':
+    for file in os.listdir(IMAGE_DIR)[0:1]:
+        filename = os.fsdecode(file)
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            image = cv2.imread(os.path.join(IMAGE_DIR, filename))
+            mask = cv2.imread(os.path.join(MASK_DIR, filename))
+
+            transform_and_save(image, mask, name="github_jfk_cropped_400", transform=crop)
