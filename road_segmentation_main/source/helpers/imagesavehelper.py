8b8a2977543a32b0ba517de84a3b82382fdf7a89
kaufmann-a <kaufmann-a@hotmail.ch>
2021-05-17 14:58:17 +0200

    Added context to img comet logging

--- road_segmentation_main/source/helpers/imagesavehelper.py
+++ road_segmentation_main/source/helpers/imagesavehelper.py
@@ -27,12 +27,13 @@ def save_predictions_to_comet(engine, loader, epoch, pixel_threshold, device, is
             # save every prediction separately
             for i in range(0, preds.shape[0]):
                 if i % 10 == 0:
-                    if nr_saves == 0:
-                        engine.comet.log_image(torchvision.transforms.ToPILImage()(x[i]),
-                                               f"{pred_img_idx}_input.png", image_format="png")
-                        engine.comet.log_image(torchvision.transforms.ToPILImage()(y[i]),
-                                               f"{pred_img_idx}_true.png", image_format="png")
-                    engine.comet.log_image(preds[i], f"{pred_img_idx}_pred_epoch_{epoch}.png", image_format="png")
+                    with engine.comet.context_manager(f"img_nr_{pred_img_idx}"):
+                        if nr_saves == 0:
+                            engine.comet.log_image(torchvision.transforms.ToPILImage()(x[i]),
+                                                   f"{pred_img_idx}_input.png", image_format="png")
+                            engine.comet.log_image(torchvision.transforms.ToPILImage()(y[i]),
+                                                   f"{pred_img_idx}_true.png", image_format="png")
+                        engine.comet.log_image(torchvision.transforms.ToPILImage()(preds[i]), f"{pred_img_idx}_pred_epoch_{epoch}.png", image_format="png")
                 pred_img_idx += 1
 
     engine.model.train()
