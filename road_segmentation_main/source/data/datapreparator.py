67d04dcf58122bc0a334fbab523eae34b040dac8
kaufmann-a <kaufmann-a@hotmail.ch>
2021-05-17 22:53:28 +0200

    Finished new dataloader to simplify loading of data across several datacollections

--- road_segmentation_main/source/data/datapreparator.py
+++ road_segmentation_main/source/data/datapreparator.py
@@ -22,11 +22,10 @@ from source.data.dataset import RoadSegmentationDataset
 class DataPreparator(object):
 
     @staticmethod
-    def load(path='', main_folder_name='', test_data_count=None):
+    def load(path=''):
         if not path:
             path = Configuration.get_path('data_collection.folder', False)
-        # if not main_folder_name:
-        #     main_folder_name = str(Configuration.get('data_collection.main_folder_name'))
+
         transform_folders = Configuration.get('data_collection.transform_folders')
         val_ratio = Configuration.get('data_collection.validation_ratio', default=0.2)
         include_val_transforms = Configuration.get('data_collection.include_val_transforms')
@@ -37,13 +36,12 @@ class DataPreparator(object):
                              for cur_collection in os.listdir(path)
                              if os.path.exists(os.path.join(path, cur_collection, cur_transformation))]
 
-        # originals_list = os.listdir(os.path.join(path, main_folder_name, 'images'))
-        # originals = [image for image in originals_list if (image.endswith('.png') or image.endswith('.jpg'))]
-
         train_set_images_orig = []
         train_set_masks_orig = []
         train_set_images_trans = []
         train_set_masks_trans = []
+
+        # Read in original imges
         for orig_folder in collections_folders_orig:
             train_set_images_orig += [os.path.join(orig_folder, "images", img) for img in
                                       os.listdir(os.path.join(orig_folder, "images"))
@@ -51,28 +49,19 @@ class DataPreparator(object):
             train_set_masks_orig += [os.path.join(orig_folder, "masks", img) for img in
                                      os.listdir(os.path.join(orig_folder, "masks"))
                                      if img.endswith('.png') or img.endswith('.jpg')]
-        for transform_folder in transform_folders:
-            train_set_images_trans += [os.path.join(transform_folder, "images", img) for img in
-                                       os.listdir(os.path.join(transform_folder, "images"))
-                                       if img.endswith('.png') or img.endswith('.jpg')]
-            train_set_masks_trans += [os.path.join(transform_folder, "masks", img) for img in
-                                      os.listdir(os.path.join(transform_folder, "masks"))
-                                      if img.endswith('.png') or img.endswith('.jpg')]
 
+        #Create mask for validation set
         train_set_images = []
         train_set_masks = []
         val_set_images = []
         val_set_masks = []
-        if include_val_transforms:
-            train_set_images_orig += train_set_images_trans
-            train_set_masks_orig += train_set_masks_trans
-
         size_val_set = int(len(train_set_images_orig) * val_ratio)
         mask = np.full(len(train_set_images_orig), False)
         mask[:size_val_set] = True
         np.random.seed(0)
         np.random.shuffle(mask)
 
+        # Define validation set
         for idx, add_to_val_set in enumerate(mask):
             if add_to_val_set:
                 val_set_images.append(train_set_images_orig[idx])
@@ -81,17 +70,32 @@ class DataPreparator(object):
                 train_set_images.append(train_set_images_orig[idx])
                 train_set_masks.append(train_set_masks_orig[idx])
 
-        if not include_val_transforms: #wrong, check val set orig if img contained there if not add to set
-            train_set_images += train_set_images_trans
-            train_set_masks += train_set_masks_trans
+        # Read all transformation images
+        for transform_folder in transform_folders:
+            train_set_images_trans += [os.path.join(transform_folder, "images", img) for img in
+                                       os.listdir(os.path.join(transform_folder, "images"))
+                                       if img.endswith('.png') or img.endswith('.jpg')]
+            train_set_masks_trans += [os.path.join(transform_folder, "masks", img) for img in
+                                      os.listdir(os.path.join(transform_folder, "masks"))
+                                      if img.endswith('.png') or img.endswith('.jpg')]
 
+        # Add transformations to training set
+        for idx, image_path in enumerate(train_set_images_trans):
+            split_path = os.path.normpath(image_path).split('\\')
+            img_name = split_path[-1]
+            collection = split_path[-4]
+            red_list = list(filter(lambda j: img_name in j, list(filter(lambda k: collection in k, val_set_images))))
+            if len(red_list) == 0:
+                train_set_images.append(image_path)
+                train_set_masks.append(train_set_masks_trans[idx])
+
+        Logcreator.info("Trainingset contains " + str(len(train_set_images)) + " images")
+        Logcreator.info("Validationset constains " + str(len(val_set_images)) + " iamges")
 
         if len(train_set_images) == 0:
             Logcreator.warn("No training files assigned.")
         if len(val_set_images) == 0:
             Logcreator.warn("No validation files assigned.")
-
-        # test images
 
         transform = get_transformations()
 
