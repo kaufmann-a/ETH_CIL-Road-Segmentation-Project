8b8d58b1bf858f609df26b32c2e8dbd7d7f82e27
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-25 15:18:25 +0200

    Added support to apply image augmentations during training

--- road_segmentation_main/source/data/transformation.py
+++ road_segmentation_main/source/data/transformation.py
@@ -5,6 +5,7 @@
 __author__ = 'Andreas Kaufmann, Jona Braun, Frederike Lübeck, Akanksha Baranwal'
 __email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, fluebeck@student.ethz.ch, abaranwal@student.ethz.ch"
 
+import os
 import sys
 
 import albumentations as A
@@ -12,6 +13,9 @@ import torch
 from albumentations.pytorch import ToTensorV2
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+
+from source.configuration import Configuration
+from source.logcreator.logcreator import Logcreator
 
 MEAN_TRAIN_IMAGES = [0.3151, 0.3108, 0.2732]
 STD_TRAIN_IMAGES = [0.1721, 0.1637, 0.1632]
@@ -58,25 +62,80 @@ def get_mean_std(dataset, debug=False):
     return mean, std
 
 
+def get_default_config():
+    try:
+        cfg = Configuration.get("data_collection.augmentations")
+    except EnvironmentError as exception:
+        # assert that only this error is ignored
+        assert (exception.args[0] == "Invalid key or wrong key specification")
+
+        class AugmentationConfig(object):
+            list = list()
+
+        cfg = AugmentationConfig()  # return empty list if no augmentations specified
+
+    return cfg
+
+
 def get_transformations(mean=None, std=None, use_train_statistics=False):
     if mean is None:
         mean = [0.0, 0.0, 0.0]
     if std is None:
         std = [1.0, 1.0, 1.0]
 
-    if use_train_statistics:
+    cfg = get_default_config()
+
+    if use_train_statistics or (hasattr(cfg, "use_train_statistics") and cfg.use_train_statistics):
         mean = MEAN_TRAIN_IMAGES
         std = STD_TRAIN_IMAGES
 
-    transform = A.Compose(
-        [
-            # A.Resize(height=400, width=400), # commented because we generally do not want to resize
-            A.Normalize(
-                mean=mean,
-                std=std,
-                max_pixel_value=255.0,
-            ),
-            ToTensorV2(),
-        ],
-    )
-    return transform
+    transform_list = list()
+
+    # dynamically load augmentations according to config
+    for method in cfg.list:
+        aug_class = eval("A." + method["name"])
+        if "params" in method:
+            aug_instance = aug_class(**method["params"])
+        else:
+            aug_instance = aug_class()
+
+        transform_list.append(aug_instance)
+
+    transform_list += [
+        # A.Resize(height=400, width=400), # commented because we generally do not want to resize
+        A.Normalize(
+            mean=mean,
+            std=std,
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2()
+    ]
+
+    transforms = A.Compose(transforms=transform_list)
+
+    Logcreator.info("Applied transformations:", transforms)
+
+    return transforms
+
+
+if __name__ == '__main__':
+    print("augmentation test run")
+    Configuration.initialize("../../configurations/default.jsonc", os.getcwd(),
+                             create_output_train=False,
+                             create_output_inf=False)
+
+    # A.CLAHE()
+    # A.ColorJitter()
+    #
+    # A.ChannelShuffle()
+    # A.ChannelDropout()
+    #
+    # A.ShiftScaleRotate()
+    # A.GaussianBlur()
+    #
+    # A.RandomFog()
+    # A.RandomContrast()
+    #
+    # A.HueSaturationValue()
+
+    get_transformations()
