dae4e65805e4896f5eba548067d7e8c9af18bb0c
braunjon <25931090+braunjon@users.noreply.github.com>
2021-05-25 22:22:48 +0200

    Update job submission script, not wokring - at least outputs commands

--- road_segmentation_main/submit_jobs.py
+++ road_segmentation_main/submit_jobs.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
+import sys
 
 if __name__ == '__main__':
 
     directory = "configurations/augmentations"
     GPU_2080TI = False
-    DEBUG = False
+    DEBUG = True
 
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
@@ -16,7 +17,7 @@ if __name__ == '__main__':
                 command += 'echo '  # test output
 
             # use 4 cpus
-            command += 'bsub -n 4 -J ' + filename[:-6]
+            command += 'bsub -n 4 -J "' + filename[:-6] + '"'
             # job time
             command += ' -W 24:00'
             # memory per cpu and select one gpu
@@ -26,9 +27,6 @@ if __name__ == '__main__':
             else:
                 command += ' -R "select[gpu_mtotal0>=10240]"'  # GPU memory more then 10GB
 
-            command += ' python train.py --configuration ' + file_path
+            command += " 'python train.py --configuration " + file_path + "'"
 
-            process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-            output, error = process.communicate()
-
-            print(output)
+            process = subprocess.run(command.split(), stdout=sys.stdout, stderr=sys.stderr)
