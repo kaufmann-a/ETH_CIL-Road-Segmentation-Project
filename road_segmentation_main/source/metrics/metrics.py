a2aa195d96902b1299f2b1d1b64c5e588bc2924a
abaranwal <abaranwal@student.ethz.ch>
2021-05-31 15:51:14 +0200

    added post processing patch metric

--- road_segmentation_main/source/metrics/metrics.py
+++ road_segmentation_main/source/metrics/metrics.py
@@ -2,6 +2,7 @@ import torch
 import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 from torchmetrics import Metric
+from source.postprocessing.postprocessing import postprocess
 
 
 class PatchAccuracy(Metric):
@@ -25,6 +26,37 @@ class PatchAccuracy(Metric):
 
         # update metric states
         self.correct += torch.sum(patched_preds == patched_target)
+        self.total += patched_target.numel()
+
+    def compute(self):
+        # compute final result
+        return self.correct.float() / self.total
+
+
+class PostProcessingPatchAccuracy(Metric):
+    def __init__(self, threshold=0.5, dist_sync_on_step=False):
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.threshold = threshold
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        # create patches
+        avgPool = torch.nn.AvgPool2d(16, stride=16)
+        patched_preds = avgPool(preds)
+        patched_target = avgPool(target)
+        # convert to integers according to threshold
+        patched_preds = (patched_preds > self.threshold).int()
+        patched_target = (patched_target > self.threshold).int()
+
+        # call post processing code
+        toimg = patched_preds.cpu().numpy()
+        toimg = toimg.astype('uint8')
+        tmp = postprocess(toimg, postprocess)
+        postprocessed_patched_preds = torch.tensor(tmp)
+
+        # update metric states
+        self.correct += torch.sum(postprocessed_patched_preds == patched_target)
         self.total += patched_target.numel()
 
     def compute(self):
