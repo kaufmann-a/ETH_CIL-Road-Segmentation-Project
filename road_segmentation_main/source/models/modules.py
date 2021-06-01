283ede598be5a56b21784b63a2d19e1bc7ead87a
braunjon <25931090+braunjon@users.noreply.github.com>
2021-06-01 18:50:14 +0200

    Reimplemented ASPP module

--- road_segmentation_main/source/models/modules.py
+++ road_segmentation_main/source/models/modules.py
@@ -17,7 +17,7 @@ class ResidualConv(nn.Module):
             nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=stride, padding=padding, bias=False),
             nn.BatchNorm2d(output_dim),
             nn.ReLU(),
-            nn.Conv2d(output_dim, output_dim, kernel_size=3, padding=1),
+            nn.Conv2d(output_dim, output_dim, kernel_size=3, padding=1, bias=False),
         )
         self.conv_skip = nn.Sequential(
             nn.Conv2d(input_dim, output_dim, kernel_size=1, stride=stride, padding=0, bias=False),
@@ -59,27 +59,25 @@ class Squeeze_Excite_Block(nn.Module):
 
 
 class ASPP(nn.Module):
+    """
+    Based on: https://github.com/rishikksh20/ResUnet/blob/master/core/modules.py
+    """
+
     def __init__(self, in_dims, out_dims, rate=[6, 12, 18]):
         super(ASPP, self).__init__()
 
         self.aspp_block1 = nn.Sequential(
-            nn.Conv2d(
-                in_dims, out_dims, 3, stride=1, padding=rate[0], dilation=rate[0]
-            ),
+            nn.Conv2d(in_dims, out_dims, 3, stride=1, padding=rate[0], dilation=rate[0]),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(out_dims),
         )
         self.aspp_block2 = nn.Sequential(
-            nn.Conv2d(
-                in_dims, out_dims, 3, stride=1, padding=rate[1], dilation=rate[1]
-            ),
+            nn.Conv2d(in_dims, out_dims, 3, stride=1, padding=rate[1], dilation=rate[1]),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(out_dims),
         )
         self.aspp_block3 = nn.Sequential(
-            nn.Conv2d(
-                in_dims, out_dims, 3, stride=1, padding=rate[2], dilation=rate[2]
-            ),
+            nn.Conv2d(in_dims, out_dims, 3, stride=1, padding=rate[2], dilation=rate[2]),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(out_dims),
         )
@@ -166,3 +164,89 @@ class PPM(nn.Module):
         for f in self.features:
             out.append(F.interpolate(f(x), x_size[2:], mode='bilinear', align_corners=True))
         return torch.cat(out, 1)
+
+
+class ASPP_new(nn.Module):
+    """
+    Atrous Spatial Pyramid Pooling - APP
+
+    Based on https://arxiv.org/pdf/1802.02611v3.pdf,
+             https://github.com/jfzhang95/pytorch-deeplab-xception/blob/master/modeling/aspp.py,
+    """
+
+    def __init__(self, in_dims, out_dims, use_bn_relu_out=False, use_global_avg_pooling=True):
+        super(ASPP_new, self).__init__()
+
+        self.out_dims = out_dims
+        self.use_global_avg_pooling = use_global_avg_pooling
+
+        kernels = [1, 3, 3, 3]
+        dilation = [1, 6, 12, 18]
+
+        # hardcoded blocks so that _init_weights() works (probably there is a better method)
+        self.aspp_block_1 = self.get_aspp_block(in_dims, out_dims, kernels[0], 0, dilation[0])
+        self.aspp_block_2 = self.get_aspp_block(in_dims, out_dims, kernels[1], dilation[1], dilation[1])
+        self.aspp_block_3 = self.get_aspp_block(in_dims, out_dims, kernels[2], dilation[2], dilation[2])
+        self.aspp_block_4 = self.get_aspp_block(in_dims, out_dims, kernels[3], dilation[3], dilation[3])
+
+        nr_blocks = len(dilation)
+
+        if self.use_global_avg_pooling:
+            self.global_avg_pool = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
+                                                 nn.Conv2d(in_dims, out_dims,
+                                                           kernel_size=(1, 1), stride=(1, 1), bias=False),
+                                                 nn.BatchNorm2d(out_dims),
+                                                 nn.ReLU())
+            nr_blocks += 1
+
+        if use_bn_relu_out:
+            self.output_block = nn.Sequential(
+                nn.Conv2d(nr_blocks * out_dims, out_dims, kernel_size=(1, 1), bias=False),
+                nn.BatchNorm2d(out_dims),
+                nn.ReLU(),
+                nn.Dropout(0.5)
+            )
+        else:
+            self.output_block = nn.Sequential(
+                nn.Conv2d(nr_blocks * out_dims, out_dims, kernel_size=(1, 1), bias=False),
+                nn.Dropout(0.5)
+            )
+
+        self._init_weights()
+
+    @staticmethod
+    def get_aspp_block(in_dims, out_dims, kernel_size, padding, dilation):
+        return nn.Sequential(
+            nn.Conv2d(in_dims, out_dims,
+                      kernel_size=(kernel_size, kernel_size),
+                      stride=(1, 1),
+                      padding=(padding, padding),
+                      dilation=(dilation, dilation),
+                      bias=False),
+            nn.BatchNorm2d(out_dims),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x1 = self.aspp_block_1(x)
+        x2 = self.aspp_block_2(x)
+        x3 = self.aspp_block_3(x)
+        x4 = self.aspp_block_4(x)
+
+        if self.use_global_avg_pooling:
+            x_gap = self.global_avg_pool(x)
+            x_gap = F.interpolate(x_gap, size=x1.size()[2:], mode='bilinear', align_corners=True)
+
+            out = torch.cat([x1, x2, x3, x4, x_gap], dim=1)
+        else:
+            out = torch.cat([x1, x2, x3, x4], dim=1)
+
+        return self.output_block(out)
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
