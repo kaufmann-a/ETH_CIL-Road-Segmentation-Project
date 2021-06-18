c20f3e9264ff46bd05a138b074c97b8fe39395a7
abaranwal <abaranwal@student.ethz.ch>
2021-06-18 17:26:16 +0200

    partial convolution unet

---
+++ road_segmentation_main/source/models/unet_pconv.py
@@ -0,0 +1,271 @@
+"""
+Unet model
+"""
+
+
+__author__ = 'Andreas Kaufmann, Jona Braun, Frederike Lübeck, Akanksha Baranwal'
+__email__ = "ankaufmann@student.ethz.ch, jonbraun@student.ethz.ch, fluebeck@student.ethz.ch, abaranwal@student.ethz.ch"
+
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+import torch.nn.functional as F
+
+from source.models.basemodel import BaseModel
+
+# Partial convolution code from https://github.com/NVIDIA/partialconv/blob/master/models/partialconv2d.py
+
+class PartialConv2d(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+
+        # whether the mask is multi-channel or not
+        if 'multi_channel' in kwargs:
+            self.multi_channel = kwargs['multi_channel']
+            kwargs.pop('multi_channel')
+        else:
+            self.multi_channel = False
+
+        if 'return_mask' in kwargs:
+            self.return_mask = kwargs['return_mask']
+            kwargs.pop('return_mask')
+        else:
+            self.return_mask = False
+
+        super(PartialConv2d, self).__init__(*args, **kwargs)
+
+        if self.multi_channel:
+            self.weight_maskUpdater = torch.ones(self.out_channels, self.in_channels, self.kernel_size[0],
+                                                 self.kernel_size[1])
+        else:
+            self.weight_maskUpdater = torch.ones(1, 1, self.kernel_size[0], self.kernel_size[1])
+
+        self.slide_winsize = self.weight_maskUpdater.shape[1] * self.weight_maskUpdater.shape[2] * \
+                             self.weight_maskUpdater.shape[3]
+
+        self.last_size = (None, None, None, None)
+        self.update_mask = None
+        self.mask_ratio = None
+
+    def forward(self, input, mask_in=None):
+        assert len(input.shape) == 4
+        if mask_in is not None or self.last_size != tuple(input.shape):
+            self.last_size = tuple(input.shape)
+
+            with torch.no_grad():
+                if self.weight_maskUpdater.type() != input.type():
+                    self.weight_maskUpdater = self.weight_maskUpdater.to(input)
+
+                if mask_in is None:
+                    # if mask is not provided, create a mask
+                    if self.multi_channel:
+                        mask = torch.ones(input.data.shape[0], input.data.shape[1], input.data.shape[2],
+                                          input.data.shape[3]).to(input)
+                    else:
+                        mask = torch.ones(1, 1, input.data.shape[2], input.data.shape[3]).to(input)
+                else:
+                    mask = mask_in
+
+                self.update_mask = F.conv2d(mask, self.weight_maskUpdater, bias=None, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=1)
+
+
+                # for mixed precision training, change 1e-8 to 1e-6
+                self.mask_ratio = self.slide_winsize / (self.update_mask + 1e-8)
+                # self.mask_ratio = torch.max(self.update_mask)/(self.update_mask + 1e-8)
+                self.update_mask = torch.clamp(self.update_mask, 0, 1)
+                self.mask_ratio = torch.mul(self.mask_ratio, self.update_mask)
+
+        raw_out = super(PartialConv2d, self).forward(torch.mul(input, mask) if mask_in is not None else input)
+
+        if self.bias is not None:
+            bias_view = self.bias.view(1, self.out_channels, 1, 1)
+            output = torch.mul(raw_out - bias_view, self.mask_ratio) + bias_view
+            output = torch.mul(output, self.update_mask)
+        else:
+            output = torch.mul(raw_out, self.mask_ratio)
+
+        if self.return_mask:
+            return output, self.update_mask
+        else:
+            return output
+
+### Code copied over from model in unet_.py
+# encoding block
+class encoding_block(nn.Module):
+    """
+    Convolutional batch norm block with relu activation (main block used in the encoding steps)
+    """
+
+    def __init__(
+            self,
+            in_size,
+            out_size,
+            kernel_size=3,
+            padding=0,
+            stride=1,
+            dilation=1,
+            batch_norm=True,
+            dropout=False,
+    ):
+        super().__init__()
+
+        if batch_norm:
+
+            # reflection padding for same size output as input (reflection padding has shown better results than zero padding)
+            layers = [
+                nn.ReflectionPad2d(padding=(kernel_size - 1) // 2),
+                PartialConv2d(
+                    in_size,
+                    out_size,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                    stride=stride,
+                    dilation=dilation,
+                ),
+                nn.PReLU(),
+                nn.BatchNorm2d(out_size),
+                nn.ReflectionPad2d(padding=(kernel_size - 1) // 2),
+                PartialConv2d(
+                    out_size,
+                    out_size,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                    stride=stride,
+                    dilation=dilation,
+                ),
+                nn.PReLU(),
+                nn.BatchNorm2d(out_size),
+            ]
+
+        else:
+            layers = [
+                nn.ReflectionPad2d(padding=(kernel_size - 1) // 2),
+                PartialConv2d(
+                    in_size,
+                    out_size,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                    stride=stride,
+                    dilation=dilation,
+                ),
+                nn.PReLU(),
+                nn.ReflectionPad2d(padding=(kernel_size - 1) // 2),
+                PartialConv2d(
+                    out_size,
+                    out_size,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                    stride=stride,
+                    dilation=dilation,
+                ),
+                nn.PReLU(),
+            ]
+
+        if dropout:
+            layers.append(nn.Dropout())
+
+        self.encoding_block = nn.Sequential(*layers)
+
+    def forward(self, input):
+        output = self.encoding_block(input)
+        return output
+
+
+# decoding block
+class decoding_block(nn.Module):
+    def __init__(self, in_size, out_size, batch_norm=False, upsampling=True):
+        super().__init__()
+
+        if upsampling:
+            self.up = nn.Sequential(
+                nn.Upsample(mode="bilinear", scale_factor=2),
+                PartialConv2d(in_size, out_size, kernel_size=1),
+            )
+
+        else:
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
+
+        self.conv = encoding_block(in_size, out_size, batch_norm=batch_norm)
+
+    def forward(self, input1, input2):
+
+        output2 = self.up(input2)
+
+        output1 = nn.functional.upsample(input1, output2.size()[2:], mode="bilinear")
+
+        return self.conv(torch.cat([output1, output2], 1))
+
+
+class UNET_PCONV(BaseModel):
+    """
+    Main UNet architecture
+    """
+    name = 'unet_pconv'
+
+    def __init__(self, config, num_classes=1):
+        super().__init__()
+
+        # encoding
+        self.conv1 = encoding_block(3, 64)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv2 = encoding_block(64, 128)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv3 = encoding_block(128, 256)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.conv4 = encoding_block(256, 512)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2)
+
+        # center
+        self.center = encoding_block(512, 1024)
+
+        # decoding
+        self.decode4 = decoding_block(1024, 512)
+        self.decode3 = decoding_block(512, 256)
+        self.decode2 = decoding_block(256, 128)
+        self.decode1 = decoding_block(128, 64)
+
+        # Choose output kernel size
+        if not config.use_submission_masks:
+            out_kernel_size = 1
+            out_stride = 1
+        else:
+            out_kernel_size = 16
+            out_stride = 16
+
+        # final
+        self.final = PartialConv2d(64, num_classes, kernel_size=out_kernel_size, stride=out_stride)
+
+    def forward(self, input):
+        # encoding
+        conv1 = self.conv1(input)
+        maxpool1 = self.maxpool1(conv1)
+
+        conv2 = self.conv2(maxpool1)
+        maxpool2 = self.maxpool2(conv2)
+
+        conv3 = self.conv3(maxpool2)
+        maxpool3 = self.maxpool3(conv3)
+
+        conv4 = self.conv4(maxpool3)
+        maxpool4 = self.maxpool4(conv4)
+
+        # center
+        center = self.center(maxpool4)
+
+        # decoding
+        decode4 = self.decode4(conv4, center)
+
+        decode3 = self.decode3(conv3, decode4)
+
+        decode2 = self.decode2(conv2, decode3)
+
+        decode1 = self.decode1(conv1, decode2)
+
+        # final
+        final = nn.functional.upsample(
+            self.final(decode1), input.size()[2:], mode="bilinear"
+        )
+
+        return final
