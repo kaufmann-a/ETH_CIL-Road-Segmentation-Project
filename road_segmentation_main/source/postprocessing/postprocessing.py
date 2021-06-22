892dfb05e777ac3270cc1f7e29cdcbf952e47a8a
akankshabaranwal <akankshabar@gmail.com>
2021-06-22 15:37:01 +0200

    creating backup

--- road_segmentation_main/source/postprocessing/postprocessing.py
+++ road_segmentation_main/source/postprocessing/postprocessing.py
@@ -1,5 +1,6 @@
 from source.postprocessing.morphology import erodedilate_v0, erodedilate_v1
 
 def postprocess(img, morphparam):
-    out_img = erodedilate_v1(img, morphparam.numdilate, morphparam.numerode, morphparam.nummedian, morphparam.kernelsz, morphparam.mediansz)
+    #out_img = erodedilate_v0(img, morphparam.numdilate, morphparam.numerode, morphparam.nummedian, morphparam.kernelsz, morphparam.mediansz)
+    out_img = img
     return out_img
